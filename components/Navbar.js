836c8f285fb69714e1a141e0a54acab9a916d395
Vinz <vinzkarloemendoza@gmail.com>
2022-06-10 16:23:36 +0800

    Remove unused link

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,15 +3,12 @@ const Navbar = () => {
   return (
     <div className="sticky top-0 z-50 px-6 py-4 backdrop-blur-lg">
       <div className="flex flex-row items-center justify-between ">
-        <div>
-          <h2 className="text-2xl">Rikai</h2>
+        <div className="flex flex-row items-baseline">
+          <h2 className="text-2xl">Rikai </h2>
         </div>
         <div className="flex flex-row space-x-4">
           <NextLink asChild href="/">
             <a>Home</a>
-          </NextLink>
-          <NextLink asChild href="/pins">
-            <a>Pins</a>
           </NextLink>
         </div>
       </div>
