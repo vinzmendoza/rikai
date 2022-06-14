fbf5998655040acf9259dd11a0793c14273ff18e
Vinz <vinzkarloemendoza@gmail.com>
2022-06-14 17:40:28 +0800

    Change navbar bg color

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import NextLink from "next/link";
 const Navbar = () => {
   return (
-    <div className="sticky top-0 z-50 px-6 py-4 backdrop-blur-lg">
+    <div className="sticky top-0 z-50 px-6 py-4 bg-neutral-900">
       <div className="flex flex-row items-center justify-between ">
         <div className="flex flex-row items-baseline">
           <h2 className="text-2xl">Rikai </h2>
