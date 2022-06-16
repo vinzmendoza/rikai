73ec8a40d3841467ae0c738f9d1d440ec87c8a61
Vinz <vinzkarloemendoza@gmail.com>
2022-06-16 17:42:09 +0800

    Change background color. Add hover style for links

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,19 @@
 import NextLink from "next/link";
+
 const Navbar = () => {
   return (
     <div className="sticky top-0 z-50 px-6 py-4 bg-neutral-900">
       <div className="flex flex-row items-center justify-between ">
         <div className="flex flex-row items-baseline">
-          <h2 className="text-2xl">Rikai </h2>
+          <NextLink asChild href="/">
+            <h2 className="text-2xl font-bold uppercase cursor-pointer hover:text-neutral-400">
+              Rikai{" "}
+            </h2>
+          </NextLink>
         </div>
         <div className="flex flex-row space-x-4">
           <NextLink asChild href="/">
-            <a>Home</a>
+            <a className="hover:underline hover:text-neutral-400">Home</a>
           </NextLink>
         </div>
       </div>
